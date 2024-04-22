e2021b8843b212d63180835592500f6eeb530a70
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-22 22:45:19 +0300

    whisout study

--- 4.py
+++ 4.py
@@ -3,3 +3,10 @@ def get_upcoming_birthdays(users):
     pass
 
 
+users = [
+    {"name": "John Doe", "birthday": "1985.04.24"},
+    {"name": "Jane Smith", "birthday": "1990.04.27"}
+]
+
+upcoming_birthdays = get_upcoming_birthdays(users)
+print("Список привітань на цьому тижні:", upcoming_birthdays)
